ed331797f5aed740796ceb8f21144dfdcbfbe8bf
mohamedosama30 <mohamedosama10013@gmail.com>
2025-09-14 04:07:16 +0300

    first Navbar

---
+++ script.js
@@ -0,0 +1,26 @@
+//   navbar
+function ShowSideBar(){
+
+    const SideBar = document.querySelector('.SideBar');
+    SideBar.style.display = 'flex'
+
+}
+
+function heidSideBar(){
+
+   const hiedsid = document.querySelector('.SideBar');
+   hiedsid.style.display = 'none'
+
+
+
+
+}
+
+
+
+
+
+
+
+
+//  End navbar
